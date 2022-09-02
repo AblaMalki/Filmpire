9193f10573ecf5474a00ff0dbfd48483ffa813de
Abla Malki <ablamalki21@gmail.com>
2022-09-02 11:57:58 +0100

    file and folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,12 +14,17 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'import/extensions': 0,
-    'import/no-cycle': 0,
+    'comma-dangle': 'off',
     'react/prop-types': 0,
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
     'max-len': [2, 250],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+    'no-underscore-dangle': [
+      'error',
+      { allow: ['_d', '_dh', '_h', '_id', '_m', '_n', '_t', '_text'] },
+    ],
     'object-curly-newline': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-one-expression-per-line': 0,
@@ -30,9 +35,14 @@ module.exports = {
     'react/no-array-index-key': 0,
     'no-param-reassign': 0,
     'react/react-in-jsx-scope': 0,
-    'react/jsx-props-no-spreading': 0,
-    'no-spare-arrays': 0,
-    'no-arrays-index-key': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to', 'hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
     camelcase: 0,
   },
 };
